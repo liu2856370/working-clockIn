31fd9ded34e5fc1b8e5448cbe6e36a226677cd3e
zren <50518861+Renato-Z@users.noreply.github.com>
2021-06-02 12:49:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -229,7 +229,7 @@ const start = async () => {
     });
   } else {
     console.log(USER_NAME)
-        console.log(process);
+        console.log(process.env.USER_NAME);
 
     return
     main();
