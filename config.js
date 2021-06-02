8c0ae03f34c2739a77faa6072e627fa6070d6557
zren <50518861+Renato-Z@users.noreply.github.com>
2021-06-02 14:17:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,25 @@
-const PUSH_PLUS_TOKEN = "fbb19de6491e4448859597359f3449e0"; //*必须修改，否则无法接收微信推送， pushPlus 的用户 token，官网：https://pushplus.hxtrip.com/
-const START_REMIND_TIME = 30; // 开始提醒签到的时间，这里只需要填写分钟，起始时间为九点，即每天9:30开始发送签到提醒
-const REMIND_INTERVAL = 300; // 提醒签到的时间间隔，单位为秒（自动查询签到结果，签到成功不在发送提醒）,设置为0则关闭签到提醒
-const CHECK_IN_LATEST_TIME = 48; // 签到的最晚时间，这里只需要填写分钟，起始时间为九点
-const WORING_TIME = 8.5; // 工作时长，即在签到时间基础上增加8.5小时打卡（实际为8.5上下的随机数并增加1小时的午休时长）
-const CHECK_Out_LATEST_TIME = {
-  // 签退的最晚时间，和工作时长互相冲突，只需开启一项
-  enable: false, // 如果开启此选项则会使用下面设定的固定时间签退
-  hours: 19,
-  minutes: 30,
+const CHECK_IN_CONFIG = {
+  ENABLE_REMIND: true, // 是否开启签到提醒
+  REMIND_INTERVAL: 5, // 签到提醒的时间间隔，单位为分钟
+  START_REMIND_TIME: 30, // 签到提醒的开始时间（填写分钟，hours默认为9，不可更改）,即每天9:30开始发送签到提醒
+  LATEST_TIME: 48, // 最后的签到时间，在此之前只会发送提醒，并且手动签到后不会再自动签到
 };
+const CHECK_OUT_CONFIG = {
+  ENABLE_REMIND: true, // 是否开启签到提醒，工作时长满8h 后开始提醒，不用设置提醒时间
+  REMIND_INTERVAL: 10, // 签到提醒的时间间隔，单位为分钟
+  LATEST_TIME: {
+    enable: false, // 如果开启此选项则会使用下面设定的固定时间签退
+    hours: 19, // 签退时间-hours
+    minutes: 30, // 签退时间-minutes
+  },
+};
+
+const WORING_TIME = 8.5; // 工作时长，即在签到时间基础上增加8.5小时打卡（实际为8.5上下的随机数并增加1小时的午休时长）
+
 const CLOCK_IN_IP = "223.70.159.3"; // 设置打卡的 IP 地址
-// sed -i '' 's/test/test/g' config.js
 module.exports = {
-  PUSH_PLUS_TOKEN,
-  START_REMIND_TIME,
-  REMIND_INTERVAL,
-  CHECK_IN_LATEST_TIME,
+  CHECK_IN_CONFIG,
+  CHECK_OUT_CONFIG,
   WORING_TIME,
   CLOCK_IN_IP,
-  CHECK_Out_LATEST_TIME,
 };
